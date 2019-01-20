73556a3a5e09f178cac02003c2619dd2a9f32a46
Jarvis <39226019+Jarfle8411@users.noreply.github.com>
2019-01-19 20:37:57 -0500

    ForEach Loop
    Foreach loop to which outputs 4 elements of an array.

---
+++ ForEachLoop.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace For_Each_Loop
+{
+    class ForEachLoop
+    {
+        static void Main(string[] args)
+        {
+            // I created a string array name Names that has 4 elements
+           string[] Names = {"John","Jack","Sam","Tammy"};
+
+            // Foreach loop which prints the names of the elements in the string Array
+           foreach (string Person in Names)
+         {
+              Console.WriteLine("Nice to meet you\n{0}", Person);
+           }
+        }
+    }
+}
